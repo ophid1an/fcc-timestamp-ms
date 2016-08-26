7a9f7ce68bb53a86f6e19aaa7df49c37df7423e6
ophid1an <dermentzoglou.ioannis@gmail.com>
2016-08-26 13:37:41 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var strftime = require('strftime');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 8000));
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/:id', function (req, res) {
+  var date = new Date(
+    isNaN(req.params.id)?
+    req.params.id:
+    (+req.params.id)*1000
+  );
+
+  console.log(typeof date, Object.keys(date));
+  var timeObj = {
+    unix: null,
+    natural: null
+  };
+
+  if (!isNaN(date.getTime())) {
+    timeObj.unix = date.getTime()/1000;
+    timeObj.natural = strftime('%B %d, %Y', date);
+  }
+
+  res.send(timeObj);
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
